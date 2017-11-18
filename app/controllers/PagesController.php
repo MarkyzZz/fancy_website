0fd226c5a53fc9a0f331f181059db5a8940bcbb0
MarkyzZ <25markyzzz@gmail.com>
2017-11-19 01:48:09 +0200

    Fixed database select for women

--- app/controllers/PagesController.php
+++ app/controllers/PagesController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controllers;
+use App\Core\App;
 /**
 *  Pages Controller class
 */
@@ -14,12 +15,28 @@ class PagesController
 	}
 
 	public function women(){
-		return view('women');
+		$product_types = App::get('database')->selectAll('product_types');
+		$sizes = App::get('database')->selectAll('sizes');
+
+		return view('women',[
+			'product_types' => $product_types,
+			'sizes' => $sizes
+		]);
 	}
 
 	public function men()
 	{
 		return view('men');
+	}
+
+	public function stock()
+	{
+		return view('stock');
+	}
+
+	public function contacts()
+	{
+		return view('contacts');
 	}
 
 	
--- routes.php
+++ routes.php
@@ -7,3 +7,5 @@ use App\Controllers\PagesController;
 $router->get('', 'PagesController@home');
 $router->get('women','PagesController@women');
 $router->get('men','PagesController@men');
+$router->get('stock','PagesController@stock');
+$router->get('contacts', 'PagesController@contacts');
